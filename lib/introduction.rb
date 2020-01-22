1214ff013f0cae10b5d459c1023cc9e97863e871
Oliver Feher <olcsee@gmail.com>
2020-01-22 04:40:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,4 +5,7 @@ end
 introduction("Oliver")
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name, "
+  puts "Hi, my name is #{name} and I am learning to program in #{language}. "
+end
+
+introduction_with_language("oliver", "english")
